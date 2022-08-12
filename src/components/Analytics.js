fa49db6754fabe51a0ca054065f01680f686546b
Squashim <konrad18v1@wp.pl>
2022-08-12 11:32:31 +0200

    55% done

--- src/components/Analytics.js
+++ src/components/Analytics.js
@@ -1,7 +1,18 @@
 import React from "react";
+import Laptop from "../assets/laptop.jpg";
 
 const Analytics = () => {
-	return <div>Analytics</div>;
+	return (
+		<div className="w-full bg-white py-16 px-4">
+			<div className="max-w-[1240px] mx-auto grid md:grid-cols-2">
+				<img src={Laptop} alt="/" />
+				<div>
+					<p>Data analytics dashboard</p>
+					<h1>Manage Data Analytics Centrally</h1>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Analytics;
