f5841eb896a8a400671a5ac2e5567370f9fb3b37
Squashim <konrad18v1@wp.pl>
2022-08-12 11:27:57 +0200

    50% done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,45 @@
 import React from "react";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
+import { useState } from "react";
 
 const Navbar = () => {
+	const [nav, setNav] = useState(false);
+
+	const handleNav = () => {
+		setNav(!nav);
+	};
+
 	return (
-		<div className="text-white">
-			<h1>REACT.</h1>
+		<div className="text-white flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4">
+			<h1 className="w-full text-3xl font-bold text-[#00df9a]">REACT.</h1>
+			<ul className="capitalize hidden md:flex">
+				<li className="p-4">home</li>
+				<li className="p-4">company</li>
+				<li className="p-4">resources</li>
+				<li className="p-4">about</li>
+				<li className="p-4">contact</li>
+			</ul>
+
+			<div onClick={handleNav} className="block md:hidden">
+				{!nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+			</div>
+
+			<div
+				className={
+					!nav
+						? "fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500"
+						: "fixed left-[-100%]"
+				}
+			>
+				<h1 className="w-full text-3xl font-bold text-[#00df9a] m-4">REACT.</h1>
+				<ul className=" uppercase p-4">
+					<li className="p-4 border-b border-gray-600">home</li>
+					<li className="p-4 border-b border-gray-600">company</li>
+					<li className="p-4 border-b border-gray-600">resources</li>
+					<li className="p-4 border-b border-gray-600">about</li>
+					<li className="p-4">contact</li>
+				</ul>
+			</div>
 		</div>
 	);
 };
