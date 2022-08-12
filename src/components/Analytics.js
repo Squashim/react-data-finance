c969f6394154ad71f29b6faa3ec299a25ff713af
Squashim <konrad18v1@wp.pl>
2022-08-12 12:23:05 +0200

    65% done

--- src/components/Analytics.js
+++ src/components/Analytics.js
@@ -5,10 +5,21 @@ const Analytics = () => {
 	return (
 		<div className="w-full bg-white py-16 px-4">
 			<div className="max-w-[1240px] mx-auto grid md:grid-cols-2">
-				<img src={Laptop} alt="/" />
-				<div>
-					<p>Data analytics dashboard</p>
-					<h1>Manage Data Analytics Centrally</h1>
+				<img src={Laptop} alt="/" className="w-[500px] mx-auto my-4" />
+				<div className="flex flex-col justify-center">
+					<p className="text-[#00df9a] font-bold">Data analytics dashboard</p>
+					<h1 className="md:text-4xl sm:text-3xl text-2xl font-bold py-2">
+						Manage Data Analytics Centrally
+					</h1>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias quas
+						accusantium id voluptate reiciendis tenetur optio saepe pariatur
+						sit? Dolores, dolore laboriosam eligendi provident corporis ipsam
+						quibusdam autem praesentium at?
+					</p>
+					<button className="bg-black w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-[#00df9a] md:mx-0">
+						Get Started
+					</button>
 				</div>
 			</div>
 		</div>
